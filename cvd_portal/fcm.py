648a64bc93edb15e98ddd5c61dbb037cac7cc049
Shreya Srivastava <shreya2feb@gmail.com>
2019-08-03 22:56:40 +0530

    Fix: New Firebase key

--- cvd_portal/fcm.py
+++ cvd_portal/fcm.py
@@ -10,7 +10,7 @@ def send_message(_to, _from, message):
     body = json.dumps(body).encode('utf8')
     headers = {
         'content-type': 'application/json',
-        'Authorization': 'key=' + 'AAAAQkMDkEA:APA91bFDkuxSwOQIKmr95_vhWwkpwO5SX4jlSUWaKTY3gxUMiT5qxN4-saBZYd-rWZL5UmNj8tl7vN9yKq32sMVzqOoRbAQUJV1xdUHllpM41JRHOpDPnRJkg-7dFpH6ZpqqaC9mwgas'
+        'Authorization': 'key=' + 'AAAA39boiz4:APA91bHyAqRkcItZ28W4TGvt3_pO8caR2l8C-0CNTEyTPupShbHvHuQTzEeXgmY8nFm_ET3TlWQpeqyui3TIBg-ObIF0boNaaGv5w55feJCw5avwtdnLIMlGSRD6oOng_1Bd1InaielO'
         }
     r = requests.post(url, data=body, headers=headers)
     print(r.text)
