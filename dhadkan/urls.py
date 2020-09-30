e2c4c270bbb12749c2ac1825f1b56517e9dd26ce
Shreya Srivastava <ssrivastava@bt.iitr.ac.in>
2020-09-30 23:11:16 +0530

    Update urls.py

--- dhadkan/urls.py
+++ dhadkan/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^dhadkan/', include('cvd_portal.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
