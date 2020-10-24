822d6dcf32059001bc0b38d272c89f1c4a7f3fe9
Durgesh <durgesh123.iitr@gmail.com>
2020-10-24 13:53:20 +0530

    Update opt.py

--- dhadkan/opt.py
+++ dhadkan/opt.py
@@ -7,15 +7,15 @@ auth_token = '4cad8d56a82780a34dc1e5c42a9d14a0' ##copy from ssrivas@bt. account
 client = Client(account_sid, auth_token) 
 
 def send_otp_msg(mobile_no, otp):
-    try:
-        content = "you otp for dhadkan password reset in this {}".format(otp)
-        message = client.messages.create( 
+    #try:
+    content = "you otp for dhadkan password reset in this {}".format(otp)
+    message = client.messages.create( 
                                     from_='+12317903233', 
                                     body = content, 
                                     to ='+91{}'.format(mobile_no)
                                 ) 
         
-        print(message.sid) 
-        return True
-    except:
-        return False
+    print(message.sid) 
+    return True
+    #except:
+     #   return False
